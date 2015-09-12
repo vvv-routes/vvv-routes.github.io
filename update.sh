a70a599f0791d7db2697afbbc74690be209780a9
Jens Steinhauser <jens.steinhauser@gmail.com>
2015-09-12 23:14:33 +0000

    add update script

---
+++ update.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -u
+set -e
+
+DIR=../bus_route_checker
+
+cd $DIR && git pull
+cd $DIR && make clean && make
+
+cp    $DIR/index.html .
+cp    $DIR/routes.json .
+cp -r $DIR/websrc 
+
+git add --all
+git commit -m "update $(date --iso-8601=seconds -u)"
+git push
